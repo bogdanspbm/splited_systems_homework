4116539009419c80ff72e0575c868737d9225e37
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-01-13 12:02:06 +0300

    Update

--- Dijkstra/src/main/java/solution/DijkstraProcessImpl.java
+++ Dijkstra/src/main/java/solution/DijkstraProcessImpl.java
@@ -2,87 +2,114 @@ package solution;
 
 import internal.Environment;
 
+import java.util.Map;
+
 public class DijkstraProcessImpl implements DijkstraProcess {
+
     private final Environment env;
 
-    private int parentID = -1;
     private int msgCounter = 0;
     private int chdCounter = 0;
-
+    private int parentID = -1;
     private long distToStart = -1;
+    private boolean isRed = false;
+    private boolean isRoot = false;
 
     public DijkstraProcessImpl(Environment env) {
         this.env = env;
     }
 
     @Override
-    public void onMessage(int senderPid, Object message) {
-        String msg = (String) message;
-
+    public void onMessage(int senderPid, Object rawMessage) {
+        String msg = (String) rawMessage;
         switch (msg) {
             case "a": {
-                msgCounter -= 1;
-                checkCanAFK();
+                this.msgCounter--;
                 break;
             }
             case "c": {
-                chdCounter += 1;
-                msgCounter -= 1;
+                this.chdCounter++;
                 break;
             }
             case "r": {
-                chdCounter -= 1;
-                checkCanAFK();
+                this.chdCounter--;
+
+                if (this.isRoot && this.checkCanAFK()) {
+                    env.finishExecution();
+                    return;
+                }
                 break;
             }
             default: {
-                Long value = Long.parseLong(msg);
-                updateDistance(senderPid, value);
-
-                if (parentID == -1) {
-                    requestChild(senderPid);
-                    doCalculation();
-                }
+                updateDistance(senderPid, Long.parseLong(msg));
             }
         }
-    }
 
-    private void checkCanAFK() {
-        if (chdCounter == 0 && msgCounter == 0) {
-            env.send(parentID, "r");
-        }
-    }
-
-    private void doCalculation() {
-        for (int key : env.getNeighbours().keySet()) {
-            env.send(key, distToStart + "");
-            msgCounter += 1;
-        }
-    }
-
-
-    private void requestChild(int toID) {
-        env.send(toID, "c");
-        parentID = toID;
-    }
-
-    private void updateDistance(int target, long value) {
-        if (distToStart == -1) {
-            distToStart = value + env.getNeighbours().get(target);
-        } else {
-            distToStart = Math.max(value + env.getNeighbours().get(target), distToStart);
-        }
+        this.tryToGoAFK();
     }
 
     @Override
     public Long getDistance() {
-        return distToStart;
+        if (this.distToStart == -1) {
+            return null;
+        } else {
+            return this.distToStart;
+        }
     }
 
     @Override
     public void onComputationStart() {
-        parentID = env.getProcessId();
-        distToStart = 0;
-        doCalculation();
+        this.distToStart = 0;
+        this.isRed = true;
+        this.isRoot = true;
+
+        for (Map.Entry<Integer, Long> entry : env.getNeighbours().entrySet()) {
+            if (env.getProcessId() == entry.getKey()) {
+                continue;
+            }
+
+            env.send(entry.getKey(), "" + entry.getValue());
+            this.msgCounter++;
+        }
+
+        if (this.msgCounter == 0) {
+            env.finishExecution();
+        }
+    }
+
+    private void updateDistance(int toID, long distance) {
+        if (!this.isRed) {
+            assert this.parentID == -1;
+            this.isRed = true;
+            this.parentID = toID;
+            env.send(toID, "c");
+        }
+
+        if (this.distToStart > distance) {
+            this.distToStart = distance;
+
+            for (Map.Entry<Integer, Long> entry : env.getNeighbours().entrySet()) {
+                if (env.getProcessId() == entry.getKey()) {
+                    continue;
+                }
+
+                env.send(entry.getKey(), "" + (this.distToStart + entry.getValue()));
+                this.msgCounter++;
+            }
+        }
+        env.send(toID, "a");
+    }
+
+    private void tryToGoAFK() {
+        if (this.isRed && this.checkCanAFK()) {
+            this.isRed = false;
+            assert this.parentID != env.getProcessId() && this.parentID != -1;
+            env.send(this.parentID, "r");
+            this.parentID = -1;
+        }
+    }
+
+    private boolean checkCanAFK() {
+        return this.chdCounter == 0 && this.msgCounter == 0;
     }
 }
